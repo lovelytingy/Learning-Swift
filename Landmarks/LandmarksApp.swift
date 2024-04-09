dbb154f6f0aa87b361893bd288b934e8a8e8a50c
Lovepreet Singh <lovelytingy@gmail.com>
2024-04-09 21:58:31 +0530

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by lovelytingy on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
